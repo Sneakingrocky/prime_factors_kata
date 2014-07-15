3597fc8c5ead67be4c778647d69cc245152091c2
Erica Garcia <garciadesign.me@gmail.com>
2014-07-15 00:21:08 -0500

    Factors 1 through 7

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -6,7 +6,10 @@ describe PrimeFactors do
     [1, []],
     [2, [2]],
     [3, [3]],
-    [4, [2, 2]]    
+    [4, [2, 2]],
+    [5, [5]],
+    [6, [2, 3]],
+    [7, [7]]     
   ].each do |number, expected|
    	it "finds all prime factors for #{number.to_s}" do
    	  expect(PrimeFactors.of(number)).to eq(expected)
