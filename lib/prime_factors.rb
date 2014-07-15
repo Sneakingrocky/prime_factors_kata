0989e268112db81970cc4136eb5a47798995db55
Erica Garcia <garciadesign.me@gmail.com>
2014-07-15 00:39:33 -0500

    Factors 1 through 9

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -2,13 +2,17 @@ class PrimeFactors
   def self.of(n)
     factors = []
     divisor = 2
-    while n%2 == 0
-      factors << 2
-      n /= 2 
-    end 
-    if n > 1
-	  factors << n
-    end
-    factors 	
+    while n > 1
+      while divisible?(n, divisor)
+        factors << divisor
+        n /= divisor 
+      end
+      divisor += 1  
+    end     
+    factors
   end
+
+  def self.divisible?(dividend, divisor)
+  	dividend % divisor == 0
+  end	
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -10,7 +10,8 @@ describe PrimeFactors do
     [5, [5]],
     [6, [2, 3]],
     [7, [7]],
-    [8, [2, 2, 2]]      
+    [8, [2, 2, 2]],
+    [9, [3, 3]]      
   ].each do |number, expected|
    	it "finds all prime factors for #{number.to_s}" do
    	  expect(PrimeFactors.of(number)).to eq(expected)
