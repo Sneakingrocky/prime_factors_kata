6c3e775773fbb2a348ffafee3e4d589ac996e58a
Erica Garcia <garciadesign.me@gmail.com>
2014-07-15 00:11:29 -0500

    Factors 1 through 4

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,8 +1,12 @@
 class PrimeFactors
   def self.of(n)
     factors = []
+    if n%2 == 0
+      factors << 2
+      n /= 2 
+    end 
     if n > 1
-    	factors << n
+	  factors << n
     end
     factors 	
   end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -5,7 +5,8 @@ describe PrimeFactors do
   [
     [1, []],
     [2, [2]],
-    [3, [3]]    
+    [3, [3]],
+    [4, [2, 2]]    
   ].each do |number, expected|
    	it "finds all prime factors for #{number.to_s}" do
    	  expect(PrimeFactors.of(number)).to eq(expected)
