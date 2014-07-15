918571eb2ac0b9930ff3c60da493223d48fdc943
Erica Garcia <garciadesign.me@gmail.com>
2014-07-14 23:29:23 -0500

    Factors 2

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,5 +1,9 @@
 class PrimeFactors
   def self.of(n)
-    []
+    factors = []
+    if n > 1
+    	factors << 2
+    end
+    factors 	
   end
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -5,4 +5,7 @@ describe PrimeFactors do
   it "finds the factor of 1" do
     expect(PrimeFactors.of(1)).to eq([])
   end
+  it "finds the factors of 2" do
+    expect(PrimeFactors.of(2)).to eq([2])
+  end 	
 end
